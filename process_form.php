c87010262c5923728d6876c13f61c20cd7e22064
FaridBourougaa <75623669+FaridBourougaa@users.noreply.github.com>
2023-12-12 11:31:41 +0100

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,11 +1,18 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $to = "csspt12@gmail.com"; // Replace with your email address
-    $subject = "New Form Submission";
+    $to = $_POST["to"];
+    $subject = $_POST["subject"];
     $message = $_POST["message"];
-    $headers = "From: " . $_POST["email"];
 
-    mail($to, $subject, $message, $headers);
-    echo "Email sent successfully.";
+    $headers = "From: your-email@example.com"; // Replace with your email address
+    $headers .= "Content-type: text/html; charset=UTF-8";
+
+    $success = mail($to, $subject, $message, $headers);
+
+    if ($success) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Error sending email. Please check your server configuration.";
+    }
 }
 ?>
