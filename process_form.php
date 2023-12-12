e355a6bb17c4d9046e83026881fc5b46e0117ba8
FaridBourougaa <75623669+FaridBourougaa@users.noreply.github.com>
2023-12-12 11:25:03 +0100

    Add files via upload

--- process_form.php
+++ process_form.php
@@ -1,15 +1,26 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $to = "your@email.com";
-    $subject = "Subject of the Email";
-    $message = "This is the content of the email.";
+	if ($_SERVER["REQUEST_METHOD"] == "POST"){
+		$firstname = $_POST["firstname"];
+		$lastname = $_POST["lastname"];
+		$children = $_POST["children"];
 
-    $headers = "From: webmaster@example.com"; // Replace with a valid email address
+		$to = "csspt12@gmail.com";
+		$subject = "معلومات التسجيل الجديدة";
+		$message = "الاسم: $firstname\nاللقب: $lastname\nعدد الأولاد: $children";
+		$headers = "From: webmaster@example.com";
+		
+		// إرسال البريد الإلكتروني
+		//mail($to, $subject, $message);
+		if (mail($to, $subject, $message, $headers)) {
+			echo "Email sent successfully.";
+		} else {
+			echo "Failed to send email.";
+		}
+		
+	}
+	else {
+		http_response_code(405);
+		echo "ERROR: Method Not Allowed";
+	}
 
-    if (mail($to, $subject, $message, $headers)) {
-        echo "Email sent successfully.";
-    } else {
-        echo "Failed to send email.";
-    }
-}
 ?>
