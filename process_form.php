e67c2e6ee9da03bc8c5a0017d49a42699293f8ae
FaridBourougaa <75623669+FaridBourougaa@users.noreply.github.com>
2023-12-12 11:27:40 +0100

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,26 +1,11 @@
 <?php
-	if ($_SERVER["REQUEST_METHOD"] == "POST"){
-		$firstname = $_POST["firstname"];
-		$lastname = $_POST["lastname"];
-		$children = $_POST["children"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "your@email.com"; // Replace with your email address
+    $subject = "New Form Submission";
+    $message = $_POST["message"];
+    $headers = "From: " . $_POST["email"];
 
-		$to = "csspt12@gmail.com";
-		$subject = "معلومات التسجيل الجديدة";
-		$message = "الاسم: $firstname\nاللقب: $lastname\nعدد الأولاد: $children";
-		$headers = "From: webmaster@example.com";
-		
-		// إرسال البريد الإلكتروني
-		//mail($to, $subject, $message);
-		if (mail($to, $subject, $message, $headers)) {
-			echo "Email sent successfully.";
-		} else {
-			echo "Failed to send email.";
-		}
-		
-	}
-	else {
-		http_response_code(405);
-		echo "ERROR: Method Not Allowed";
-	}
-
+    mail($to, $subject, $message, $headers);
+    echo "Email sent successfully.";
+}
 ?>
